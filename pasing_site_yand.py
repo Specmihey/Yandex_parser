1442b6b3aa51ead5ca400761a582ef88822d1768
Mikhail Chizhov <specmihey@gmail.com>
2018-09-16 14:47:38 +0300

    pasing_site_yand_v2_1
    Version 2.1 added a description of the page

--- pasing_site_yand.py
+++ pasing_site_yand.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Sep 16 11:45:15 2018
-Парсинг всех страниц сайта с получением алиасов и title страниц из индекса Яндекс V2
+Парсинг всех страниц сайта с получением алиасов и title страниц из индекса Яндекс V2.1
+Парсится:Title, Description, URL, находящиеся в индексе Яндекса.
 @author: @specmihey
 """
-url_site = 'http://www.ymc2003.ru/'
+url_site = 'http://www.ymc2003.ru/' #The test site
 
 import os
-os.chdir('C:\\Users\\user\\Desktop\\Python\\Парсинг сайта алиасы') 
+os.chdir('C:\\Users\\user\\Desktop\\Python\\Парсинг сайта алиасы') #Your working directory
 import urllib.request
 import requests
 import re
@@ -34,15 +35,19 @@ browser.get(baseU)
 page_count = 0
 url_pages = []   
 titlePages = [] 
+descrPages = []
 while True:
     page_count += 1
     time.sleep(1)
     urlNum = browser.find_elements_by_class_name('organic__url') #len(urlNum)
     titleP = browser.find_elements_by_class_name('organic__url-text') #len(titleP)
+    desPages = browser.find_elements_by_class_name('organic__text')
     for p in urlNum:
         url_pages.append(p.get_attribute('href'))
     for l in titleP:
         titlePages.append(l.text)
+    for mi in desPages:
+        descrPages.append(mi.text)
     try:
         # Clicking on "2" on pagination on first iteration, "3" on second...
         browser.find_element_by_link_text('дальше').click()
@@ -50,16 +55,17 @@ while True:
         # Stop loop if no more page available
         break
 
-# ---- Write the data to the file 'site.xls'
+# ---- Write data to a file
 url_set = pd.DataFrame(url_pages)        
 title_set = pd.DataFrame(titlePages)
+descr_set = pd.DataFrame(descrPages)
 browser.quit() 
 UT = pd.DataFrame()
-UT = pd.concat([title_set,url_set],sort=False,axis=1)
-UT.columns = ['Title','URL']
+UT = pd.concat([title_set,descr_set,url_set],sort=False,axis=1)
+UT.columns = ['Title','Description','URL']
 UT.to_excel('site.xls', index=False)  
 import winsound
-winsound.MessageBeep()   
+winsound.MessageBeep() 
 
 
 
